e614f7492074a034566a50830fd605d33dbf2462
Trent Kan <trent.kan@yello.co>
2017-10-02 14:45:52 -0500

    Move adjust_quality_at_start into Item class

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -11,7 +11,7 @@ class GildedRose
 
   def update_quality
     items.each do |item|
-      adjust_quality_at_start_for(item)
+      item.adjust_quality_at_start
 
       decrement_sell_in_for(item)
 
@@ -21,23 +21,7 @@ class GildedRose
 
   private
 
-  def adjust_quality_at_start_for(item)
-    if item.name != Item::AGED_BRIE and item.name != Item::BACKSTAGE_PASSES
-      decrement_quality_for(item)
-    else
-      if item.name == Item::BACKSTAGE_PASSES
-        if item.sell_in >= 11
-          increment_quality_for(item, 1)
-        elsif item.sell_in < 11 && item.sell_in >=6
-          increment_quality_for(item, 2)
-        else
-          increment_quality_for(item, 3)
-        end
-      else
-        increment_quality_for(item, 1)
-      end
-    end
-  end
+
 
   def adjust_quality_after_sell_in_decremented_for(item)
     if item.sell_in < 0
@@ -84,13 +68,75 @@ class Item
   AGED_BRIE = "Aged Brie".freeze
   SULFURAS = "Sulfuras, Hand of Ragnaros".freeze
 
+  MIN_QUALITY = 0.freeze
+  MAX_QUALITY = 50.freeze
+
   def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = quality
   end
 
+  def adjust_quality_at_start_for(item)
+    puts "name: #{name}, sell_in: #{sell_in}, quality: #{quality}"
+    if item.name != Item::AGED_BRIE and item.name != Item::BACKSTAGE_PASSES
+      decrement_quality_for(item)
+    else
+      if item.name == Item::BACKSTAGE_PASSES
+        if item.sell_in >= 11
+          increment_quality_for(item, 1)
+        elsif item.sell_in < 11 && item.sell_in >=6
+          increment_quality_for(item, 2)
+        else
+          increment_quality_for(item, 3)
+        end
+      else
+        increment_quality_for(item, 1)
+      end
+    end
+  end
+
+  def adjust_quality_at_start
+    if name != AGED_BRIE and name != BACKSTAGE_PASSES
+      decrement_quality_by(1)
+    else
+      if name == BACKSTAGE_PASSES
+        if sell_in >= 11
+          increment_quality_by(1)
+        elsif sell_in < 11 && sell_in >=6
+          increment_quality_by(2)
+        else
+          increment_quality_by(3)
+        end
+      else
+        increment_quality_by(1)
+      end
+    end
+  end
+
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+
+  private
+
+  def decrement_quality_by(decremented_value)
+    decrement_attribute(:quality, decremented_value) if quality > MIN_QUALITY
+  end
+
+  def decrement_attribute(attribute, decremented_value)
+    if name != SULFURAS
+      decremented_attribute = public_send(attribute) - decremented_value
+      instance_variable_as_symbol = "@#{attribute}".to_sym
+
+      instance_variable_set(instance_variable_as_symbol, decremented_attribute)
+    end
+  end
+
+  def increment_quality_by(incremented_value)
+    if quality < MAX_QUALITY
+      new_quality = quality + incremented_value
+      self.quality = new_quality <= 50 ? new_quality : 50
+    end
   end
 end
