8adda69d4bd943a05182b615dfb1c66dc4a66bd9
Trent Kan <trent.kan@yello.co>
2017-10-02 12:39:10 -0500

    Create acceptance test

---
+++ golden_master.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+for iteration in {0..15}
+do
+  ruby texttest_fixture.rb > spec/temp_files/day_"$iteration".txt
+  if [ "$1" == "--override" ]; then
+    cp spec/temp_files/day_"$iteration".txt spec/master_files/day_"$iteration".txt
+  fi
+
+  DIFF=$(diff spec/temp_files/day_"$iteration".txt spec/master_files/day_"$iteration".txt)
+
+  if [ "$DIFF" != "" ]
+  then
+    echo Failed iteration "$iteration"
+    diff spec/temp_files/day_"$iteration".txt spec/master_files/day_"$iteration".txt
+  else
+    echo Passed iteration "$iteration"
+  fi
+  echo
+  echo
+done
